ec70d65dbe5a88e868def30c1b1ba1eaf167fbd7
Marcelo Maico <mmaico@gmail.com>
2022-04-12 19:07:53 -0300

    Adding custom message sender.

--- src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/KMessageSender.kt
+++ src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/KMessageSender.kt
@@ -1,20 +1,25 @@
-package harpo.network.communication.p2p.infrastructure
+package harpo.network.communication.p2p.infrastructure.messages
 
+import harpo.network.communication.p2p.infrastructure.ConnectionInfoImpl
+import harpo.network.communication.p2p.infrastructure.messages.type.Type
+import harpo.network.communication.p2p.infrastructure.messages.type.Type.valueOf
 import io.ep2p.kademlia.connection.MessageSender
 import io.ep2p.kademlia.node.KademliaNodeAPI
 import io.ep2p.kademlia.node.Node
 import io.ep2p.kademlia.protocol.message.KademliaMessage
 import java.io.Serializable
 import java.math.BigDecimal
+import java.net.Proxy
 
 class KMessageSender: MessageSender<BigDecimal, ConnectionInfoImpl> {
 
-    override fun <I : Serializable?, O : Serializable?> sendMessage(
+    override fun <INPUT : Serializable?, OUTPUT : Serializable?> sendMessage(
         api: KademliaNodeAPI<BigDecimal, ConnectionInfoImpl>?,
         external: Node<BigDecimal, ConnectionInfoImpl>?,
-        message: KademliaMessage<BigDecimal, ConnectionInfoImpl, O>?
-    ): KademliaMessage<BigDecimal, ConnectionInfoImpl, I> {
+        message: KademliaMessage<BigDecimal, ConnectionInfoImpl, OUTPUT>?
+    ): KademliaMessage<BigDecimal, ConnectionInfoImpl, INPUT> {
 
+        valueOf(message?.type!!)
         //use the grpc to send the message to external node
 
         TODO("Not yet implemented")
