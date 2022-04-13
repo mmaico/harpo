b7beb426ce0e3d35005bea270cdf5be833f67ac0
Marcelo Maico <mmaico@gmail.com>
2022-04-13 12:39:59 -0300

    Changed the structure of message

--- src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/type/Message.kt
+++ src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/type/Message.kt
@@ -1,4 +1,19 @@
 package harpo.network.communication.p2p.infrastructure.messages.type
 
-sealed class Message(val type: Type)
+import harpo.network.communication.p2p.infrastructure.ConnectionInfoImpl
+import io.ep2p.kademlia.node.KademliaNodeAPI
+import io.ep2p.kademlia.node.Node
+import io.ep2p.kademlia.protocol.message.KademliaMessage
+import java.io.Serializable
+import java.math.BigDecimal
+
+interface Message {
+
+    fun <INPUT : Serializable?, OUTPUT : Serializable?> sendMessage(
+        api: KademliaNodeAPI<BigDecimal, ConnectionInfoImpl>?,
+        external: Node<BigDecimal, ConnectionInfoImpl>?,
+        message: KademliaMessage<BigDecimal, ConnectionInfoImpl, OUTPUT>?
+    ): KademliaMessage<BigDecimal, ConnectionInfoImpl, INPUT>
+
+}
 
