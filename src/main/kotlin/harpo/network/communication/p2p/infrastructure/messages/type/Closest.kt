84773d7bae9b05da36142c9ca95e6c1e4ab5547a
Marcelo Maico <mmaico@gmail.com>
2022-04-14 18:05:43 -0300

    Fixed closest and ping event handler.

--- src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/type/Closest.kt
+++ src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/type/Closest.kt
@@ -1,40 +1,43 @@
 package harpo.network.communication.p2p.infrastructure.messages.type
 
-import harpo.infrastructure.DateHelper
 import harpo.infrastructure.DateHelper.now
 import harpo.network.communication.p2p.infrastructure.ConnectionInfoImpl
 import harpo.network.communication.p2p.infrastructure.grpc.repository.OperationsRepository
 import harpo.network.communication.p2p.infrastructure.kademlia.ExternalNode
+import io.ep2p.kademlia.model.FindNodeAnswer
 import io.ep2p.kademlia.node.KademliaNodeAPI
 import io.ep2p.kademlia.node.Node
+import io.ep2p.kademlia.node.external.BigIntegerExternalNode
+import io.ep2p.kademlia.protocol.message.FindNodeResponseMessage
 import io.ep2p.kademlia.protocol.message.KademliaMessage
 import java.io.Serializable
-import java.math.BigDecimal
-import java.util.*
-import io.ep2p.kademlia.protocol.message.FindNodeResponseMessage
 import java.math.BigInteger
 
 class Closest(private val repository: OperationsRepository): Message {
 
     override fun <INPUT : Serializable?, OUTPUT : Serializable?> sendMessage(
-        self: KademliaNodeAPI<BigDecimal, ConnectionInfoImpl>?,
-        receiver: Node<BigDecimal, ConnectionInfoImpl>?,
-        message: KademliaMessage<BigDecimal, ConnectionInfoImpl, OUTPUT>?
-    ): KademliaMessage<BigDecimal, ConnectionInfoImpl, INPUT> {
+        self: KademliaNodeAPI<BigInteger, ConnectionInfoImpl>?,
+        receiver: Node<BigInteger, ConnectionInfoImpl>?,
+        message: KademliaMessage<BigInteger, ConnectionInfoImpl, OUTPUT>?
+    ): KademliaMessage<BigInteger, ConnectionInfoImpl, INPUT> {
 
         val closest = this.repository.closest(receiver, self)
-        val externalNodes = closest.parallelStream().map {
-            ExternalNode(
+
+        val nodeAnswer = FindNodeAnswer<BigInteger, ConnectionInfoImpl>(self?.id)
+
+        closest.parallelStream().forEach {
+            val externalNode = ExternalNode(
                 _connectionInfo = ConnectionInfoImpl(it.contact.ip, it.contact.port),
                 _id = it.id.toBigInteger(),
                 _lastSeen = now()
             )
+            nodeAnswer.add(BigIntegerExternalNode(externalNode, BigInteger.ONE))
         }
 
         val responseMessage = FindNodeResponseMessage<BigInteger, ConnectionInfoImpl>()
-        responseMessage.
+        responseMessage.data = nodeAnswer
 
-        TODO()
+        return responseMessage as KademliaMessage<BigInteger, ConnectionInfoImpl, INPUT>
     }
 
 }
--- src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/type/Ping.kt
+++ src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/type/Ping.kt
@@ -8,22 +8,23 @@ import io.ep2p.kademlia.protocol.message.KademliaMessage
 import io.ep2p.kademlia.protocol.message.PongKademliaMessage
 import java.io.Serializable
 import java.math.BigDecimal
+import java.math.BigInteger
 
 class Ping(private val repository: OperationsRepository): Message {
 
     override fun <INPUT : Serializable?, OUTPUT : Serializable?> sendMessage(
-        self: KademliaNodeAPI<BigDecimal, ConnectionInfoImpl>?,
-        receiver: Node<BigDecimal, ConnectionInfoImpl>?,
-        message: KademliaMessage<BigDecimal, ConnectionInfoImpl, OUTPUT>?
-    ): KademliaMessage<BigDecimal, ConnectionInfoImpl, INPUT> {
+        self: KademliaNodeAPI<BigInteger, ConnectionInfoImpl>?,
+        receiver: Node<BigInteger, ConnectionInfoImpl>?,
+        message: KademliaMessage<BigInteger, ConnectionInfoImpl, OUTPUT>?
+    ): KademliaMessage<BigInteger, ConnectionInfoImpl, INPUT> {
 
         val answer = repository.ping(receiver, self)
 
-        val response = PongKademliaMessage<BigDecimal, ConnectionInfoImpl>()
+        val response = PongKademliaMessage<BigInteger, ConnectionInfoImpl>()
         response?.node = receiver
         response?.isAlive = answer.isAlive
 
-        return response as KademliaMessage<BigDecimal, ConnectionInfoImpl, INPUT>
+        return response as KademliaMessage<BigInteger, ConnectionInfoImpl, INPUT>
     }
 
 }
