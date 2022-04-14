d2b4951367a33f86e28db5fb57c36f412ae35919
Marcelo Maico <mmaico@gmail.com>
2022-04-14 17:22:55 -0300

    Change losest

--- src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/type/Closest.kt
+++ src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/type/Closest.kt
@@ -1,12 +1,18 @@
 package harpo.network.communication.p2p.infrastructure.messages.type
 
+import harpo.infrastructure.DateHelper
+import harpo.infrastructure.DateHelper.now
 import harpo.network.communication.p2p.infrastructure.ConnectionInfoImpl
 import harpo.network.communication.p2p.infrastructure.grpc.repository.OperationsRepository
+import harpo.network.communication.p2p.infrastructure.kademlia.ExternalNode
 import io.ep2p.kademlia.node.KademliaNodeAPI
 import io.ep2p.kademlia.node.Node
 import io.ep2p.kademlia.protocol.message.KademliaMessage
 import java.io.Serializable
 import java.math.BigDecimal
+import java.util.*
+import io.ep2p.kademlia.protocol.message.FindNodeResponseMessage
+import java.math.BigInteger
 
 class Closest(private val repository: OperationsRepository): Message {
 
@@ -17,8 +23,18 @@ class Closest(private val repository: OperationsRepository): Message {
     ): KademliaMessage<BigDecimal, ConnectionInfoImpl, INPUT> {
 
         val closest = this.repository.closest(receiver, self)
+        val externalNodes = closest.parallelStream().map {
+            ExternalNode(
+                _connectionInfo = ConnectionInfoImpl(it.contact.ip, it.contact.port),
+                _id = it.id.toBigInteger(),
+                _lastSeen = now()
+            )
+        }
 
-        return
+        val responseMessage = FindNodeResponseMessage<BigInteger, ConnectionInfoImpl>()
+        responseMessage.
+
+        TODO()
     }
 
 }
