06461929a8b625bd9d84d256352d6cc20854bbbb
Marcelo Maico <mmaico@gmail.com>
2022-04-14 17:23:49 -0300

    Created external node to send to api

---
+++ src/main/kotlin/harpo/network/communication/p2p/infrastructure/kademlia/ExternalNode.kt
@@ -0,0 +1,22 @@
+package harpo.network.communication.p2p.infrastructure.kademlia
+
+import harpo.network.communication.p2p.infrastructure.ConnectionInfoImpl
+import io.ep2p.kademlia.node.Node
+import java.math.BigInteger
+import java.util.*
+
+class ExternalNode(private val _connectionInfo: ConnectionInfoImpl, private val _id: BigInteger, private var _lastSeen: Date?): Node<BigInteger, ConnectionInfoImpl> {
+
+
+    override fun getConnectionInfo(): ConnectionInfoImpl = _connectionInfo
+
+    override fun getId(): BigInteger = this._id
+
+    override fun setLastSeen(date: Date?) {
+        this._lastSeen = date
+    }
+
+    override fun getLastSeen(): Date? {
+        return this.lastSeen
+    }
+}
--- src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/type/Ping.kt
+++ src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/type/Ping.kt
@@ -1,9 +1,6 @@
 package harpo.network.communication.p2p.infrastructure.messages.type
 
-import harpo.network.communication.p2p.KademliaServiceGrpc
-import harpo.network.communication.p2p.domain.model.Contact
 import harpo.network.communication.p2p.infrastructure.ConnectionInfoImpl
-import harpo.network.communication.p2p.infrastructure.grpc.client.ChannelPool
 import harpo.network.communication.p2p.infrastructure.grpc.repository.OperationsRepository
 import io.ep2p.kademlia.node.KademliaNodeAPI
 import io.ep2p.kademlia.node.Node
@@ -11,8 +8,6 @@ import io.ep2p.kademlia.protocol.message.KademliaMessage
 import io.ep2p.kademlia.protocol.message.PongKademliaMessage
 import java.io.Serializable
 import java.math.BigDecimal
-import harpo.network.communication.p2p.Contact as ContactGRPC
-import harpo.network.communication.p2p.Node as NodeGRPC
 
 class Ping(private val repository: OperationsRepository): Message {
 
@@ -25,8 +20,8 @@ class Ping(private val repository: OperationsRepository): Message {
         val answer = repository.ping(receiver, self)
 
         val response = PongKademliaMessage<BigDecimal, ConnectionInfoImpl>()
-        response.node = receiver
-        response.isAlive = answer.isAlive
+        response?.node = receiver
+        response?.isAlive = answer.isAlive
 
         return response as KademliaMessage<BigDecimal, ConnectionInfoImpl, INPUT>
     }
