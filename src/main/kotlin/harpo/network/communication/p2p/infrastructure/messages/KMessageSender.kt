7689395ad991691c4d38fe1d7a77e817dc2b058b
Marcelo Maico <peo_mjesus@uolinc.com>
2022-04-16 07:40:43 -0300

    Implemented the generic message sender.

--- src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/KMessageSender.kt
+++ src/main/kotlin/harpo/network/communication/p2p/infrastructure/messages/KMessageSender.kt
@@ -9,27 +9,34 @@ import io.ep2p.kademlia.node.Node
 import io.ep2p.kademlia.protocol.message.KademliaMessage
 import java.io.Serializable
 import java.math.BigDecimal
+import java.math.BigInteger
 import java.net.Proxy
 
-class KMessageSender: MessageSender<BigDecimal, ConnectionInfoImpl> {
+class KMessageSender: MessageSender<BigInteger, ConnectionInfoImpl> {
 
     override fun <INPUT : Serializable?, OUTPUT : Serializable?> sendMessage(
-        self: KademliaNodeAPI<BigDecimal, ConnectionInfoImpl>?,
-        receiver: Node<BigDecimal, ConnectionInfoImpl>?,
-        message: KademliaMessage<BigDecimal, ConnectionInfoImpl, OUTPUT>?
-    ): KademliaMessage<BigDecimal, ConnectionInfoImpl, INPUT> {
+        self: KademliaNodeAPI<BigInteger, ConnectionInfoImpl>?,
+        receiver: Node<BigInteger, ConnectionInfoImpl>?,
+        message: KademliaMessage<BigInteger, ConnectionInfoImpl, OUTPUT>?
+    ): KademliaMessage<BigInteger, ConnectionInfoImpl, INPUT> {
 
-        valueOf(message?.type!!)
-        //use the grpc to send the message to external node
-
-        TODO("Not yet implemented")
+        val type = Type.valueOf(message?.type!!)
+        val messageRegistered = MessageFactory.getMessageBy(type)
+        return messageRegistered?.sendMessage(self, receiver, message)
+            ?: message as KademliaMessage<BigInteger, ConnectionInfoImpl, INPUT>
     }
 
     override fun <O : Serializable?> sendAsyncMessage(
-        api: KademliaNodeAPI<BigDecimal, ConnectionInfoImpl>?,
-        p1: Node<BigDecimal, ConnectionInfoImpl>?,
-        p2: KademliaMessage<BigDecimal, ConnectionInfoImpl, O>?) {
-        TODO("Not supported yet")
+        self: KademliaNodeAPI<BigInteger, ConnectionInfoImpl>?,
+        receiver: Node<BigInteger, ConnectionInfoImpl>?,
+        message: KademliaMessage<BigInteger, ConnectionInfoImpl, O>?
+    ) {
+        val type = Type.valueOf(message?.type!!)
+
+        val messageRegistered = MessageFactory.getMessageBy(type)
+
+        //messageRegistered?.sendMessage(self, receiver, message)
+        TODO("No implemented yet")
     }
 
 
